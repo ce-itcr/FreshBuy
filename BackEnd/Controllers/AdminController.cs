eb8d1752d3e592d390ca4f0a6b20f11e3b7a174d
Angelo Ortiz Vega <angelo2898@gmail.com>
2020-10-08 19:12:05 -0600

    [ADD] Test Backend Attempt 2

---
+++ BackEnd/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace BackEnd.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200/", headers: "*", methods: "*")]
+    public class AdminController : ApiController
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Debug.Print("Hola");
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [HttpPost]
+        [Route("api/Admin/Producer/addtest2")]
+        public IHttpActionResult AddProducer([FromBody] Object new_producer)
+        {
+            bool result = true;
+            if (!result)
+            {
+                return NotFound();
+            }
+            Debug.Print(new_producer.ToString());
+            return Ok(new_producer);
+        }
+
+
+    }
+}
